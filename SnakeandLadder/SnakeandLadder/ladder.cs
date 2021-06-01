e854d8b5aa69511943b57d03df49425cc79db49a
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-01 18:50:00 +0530

    [Rishi]Adding:SnakeorLadder

--- SnakeandLadder/SnakeandLadder/ladder.cs
+++ SnakeandLadder/SnakeandLadder/ladder.cs
@@ -14,6 +14,39 @@ namespace SnakeandLadder
             Random random = new Random();
             int die = random.Next(1, 6);
             Console.WriteLine("Your number is:" + die);
+            Console.WriteLine("u want continue the game y/n");
+            String value = Convert.ToString(Console.ReadLine());
+            if (value == "y")
+         
+            {
+                int answer = random.Next(1, 2);
+                switch(answer)
+                {
+                    case 1:
+                        position = position + die;
+                        Console.WriteLine("You get a ladder step to" + position);
+                        break;
+                    case 2:
+                        if (position < 0) ;
+                        {
+                            Console.WriteLine("You lost the game");
+                        }
+                        
+                        else
+                        {
+                            position = position - die;
+                            
+                        }
+                        Console.WriteLine("......Snake bits>>>> you" + position);
+                        break;
+                    default:
+                        Console.WriteLine("NO position");
+                        break;
+
+
+                }
+
+            }
 
         }
     }
