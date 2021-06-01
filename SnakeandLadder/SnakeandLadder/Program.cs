416e49155ba199c29e55c856018742d7e89fec8b
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-01 18:04:04 +0530

    [Rishi]ADDING:PositionZero

---
+++ SnakeandLadder/SnakeandLadder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeandLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wlecome to sanke and ladder game");
+            ladder Ladder = new ladder();
+            Ladder.game();
+
+        }
+    }
+}
---
+++ SnakeandLadder/SnakeandLadder/ladder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeandLadder
+{
+    class ladder
+    {
+        public void game()
+        {
+            Console.WriteLine("Welcome to snake and ladder game ");
+            int position = 0;
+            Console.WriteLine("player position is" + position);
+
+        }
+    }
+}
